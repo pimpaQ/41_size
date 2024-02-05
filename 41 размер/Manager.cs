204f291eb195a8ffb12b02bd524f647cc27ac6e0
jamid <jamid@DESKTOP-E8C6K79>
2024-02-05 11:57:14 +0500

    Добавьте файлы проекта.

---
+++ 41 размер/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace _41_размер
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
